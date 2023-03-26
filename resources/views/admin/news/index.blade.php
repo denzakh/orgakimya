04f3824fee9534552a2eb1e10546f40907ce820d
Denis Zakharchenko <den.zakh@gmail.com>
2023-03-26 21:31:57 +0300

    add admin lte

---
+++ resources/views/admin/news/index.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title', 'News')
+
+@section('content_header')
+    <h1>News</h1>
+@stop
+
+@section('content')
+    111
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,10 +32,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
+Route::middleware('auth')->group(function () {
+	Route::get('/admin', [AdminController::class, 'index']);
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
 
 require __DIR__.'/auth.php';
