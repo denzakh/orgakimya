3c9cac2ee71fa9bf8af21ea05033e4d0f46b9710
Denis Zakharchenko <den.zakh@gmail.com>
2023-03-28 20:32:38 +0300

    active menu

--- resources/views/layouts/partials/nav-item.blade.php
+++ resources/views/layouts/partials/nav-item.blade.php
@@ -1,5 +1,13 @@
+@php 
+	$isActive = LaravelLocalization::localizeUrl($item['url']) === url()->current();
+@endphp 
+
+
 <x-nav-link 
     :href="LaravelLocalization::localizeUrl($item['url'])" 
+    :active="$isActive"
 >
     {{ __($item['title']) }}
+
+    {{ request()->routeIs(LaravelLocalization::localizeUrl($item['url']))  }}
 </x-nav-link>
