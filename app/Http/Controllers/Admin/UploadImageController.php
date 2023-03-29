bcfc5591274ddd3fead5cfde26e6544343540004
Denis Zakharchenko <den.zakh@gmail.com>
2023-03-29 21:51:20 +0300

    create_sized_images helper

--- app/Http/Controllers/Admin/UploadImageController.php
+++ app/Http/Controllers/Admin/UploadImageController.php
@@ -6,10 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Photo;
 
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
-
-use App\Helpers\PhotoHelper::createSizedImages();
- 
+use Intervention\Image\Facades\Image; 
  
 class UploadImageController extends Controller
 {
@@ -21,67 +18,26 @@ class UploadImageController extends Controller
     public function save(Request $request)
     {
 
-    	// Подготавливаем данные
         $data = $request->all();
         $fileObj = $data['image'];
 
         $options = [
     		'files' => [$fileObj],
-    		// 'sizes' => [
-    		// 	'normal' => 1200,
-    		// 	'thumbnail' => 640,
-    		// ],
-    		// 'catalog' => 'shared',
-    		// 'isWebp' => true,
-    		// 'quality' => 75, 
+    		'sizes' => [
+    			'normal' => 1200,
+    			'thumbnail' => 640,
+    		],
+    		'catalog' => 'shared',
+    		'isWebp' => true,
+    		'quality' => 75, 
     	];
 
-    	$result = createSizedImages($options);
+    	$result = [];
 
-    	$var_dump($result);
+    	if (function_exists('create_sized_images')) {
+	    	$result = create_sized_images($options);
+	    }
 
-		die();
-
-
-
-  //       $filename    = $fileObj->getClientOriginalName(); //image_file is the name of the file field used in the form in blade
-  //       $originalExtension = '.'.$fileObj->getClientOriginalExtension(); //
-  //       $newFilename =  time(); //.$filename;
-
-        
-
-		
-  //       //Сохраняем оригинальную картинку
-  //       $fileObj->move(Storage::path('/public/images/').'origin/',$newFilename.$originalExtension);
-
-
-		// //Создаем обычный вариант
-  //       $img = Image::make(Storage::path('/public/images/').'origin/'.$newFilename.$originalExtension);
-  //       $img->fit(1200, 1200); // обрезаем до
-		// $img->save(Storage::path('/public/images/').'normal/'.$newFilename.$originalExtension, 75);         
-
-  //       // webp
-  //       $imgWebp = $img->encode('webp', 75);  // 75 is image quality and its value can be 1 to 100
-		// $imgWebp->save(Storage::path('/public/images/').'normal/'.$newFilename.'.webp'); // 75 is the image quality
-
-
-  //       //Создаем миниатюру изображения и сохраняем ее
-  //       $thumbnail = Image::make(Storage::path('/public/images/').'origin/'.$newFilename.$originalExtension);
-  //       $thumbnail->fit(640, 640);
-  //       $thumbnail->save(Storage::path('/public/images/').'thumbnail/'.$newFilename.$originalExtension, 75);// 75 is the image quality
-
-  //       // webp
-  //       $thumbnailWebp = $img->encode('webp', 75);  // 75 is image quality and its value can be 1 to 100
-		// $thumbnailWebp->save(Storage::path('/public/images/').'thumbnail/'.$newFilename.'.webp'); // 75 is the image quality
-
-        // $save = new Photo;
- 
-        // $save->name = $filename;
-        // $save->path = $path;
- 
-        // $save->save();
- 
         return redirect('admin/upload-image')->with('status', 'Изображение было загружено');
- 
     }
 }
