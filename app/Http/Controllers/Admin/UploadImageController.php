1cd9ff02e7ebe6e09bced9b2c982ce75af28153a
Denis Zakharchenko <den.zakh@gmail.com>
2023-03-29 20:36:57 +0300

    class helper 1

--- app/Http/Controllers/Admin/UploadImageController.php
+++ app/Http/Controllers/Admin/UploadImageController.php
@@ -7,6 +7,8 @@ use App\Models\Photo;
 
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+
+use App\Helpers\PhotoHelper::createSizedImages();
  
  
 class UploadImageController extends Controller
@@ -22,35 +24,55 @@ class UploadImageController extends Controller
     	// Подготавливаем данные
         $data = $request->all();
         $fileObj = $data['image'];
-        $filename    = $fileObj->getClientOriginalName(); //image_file is the name of the file field used in the form in blade
-        $originalExtension = '.'.$fileObj->getClientOriginalExtension(); //
-        $newFilename =  time(); //.$filename;
+
+        $options = [
+    		'files' => [$fileObj],
+    		// 'sizes' => [
+    		// 	'normal' => 1200,
+    		// 	'thumbnail' => 640,
+    		// ],
+    		// 'catalog' => 'shared',
+    		// 'isWebp' => true,
+    		// 'quality' => 75, 
+    	];
+
+    	$result = createSizedImages($options);
+
+    	$var_dump($result);
+
+		die();
+
+
+
+  //       $filename    = $fileObj->getClientOriginalName(); //image_file is the name of the file field used in the form in blade
+  //       $originalExtension = '.'.$fileObj->getClientOriginalExtension(); //
+  //       $newFilename =  time(); //.$filename;
 
         
 
 		
-        //Сохраняем оригинальную картинку
-        $fileObj->move(Storage::path('/public/images/').'origin/',$newFilename.$originalExtension);
+  //       //Сохраняем оригинальную картинку
+  //       $fileObj->move(Storage::path('/public/images/').'origin/',$newFilename.$originalExtension);
 
 
-		//Создаем обычный вариант
-        $img = Image::make(Storage::path('/public/images/').'origin/'.$newFilename.$originalExtension);
-        $img->fit(1200, 1200); // обрезаем до
-		$img->save(Storage::path('/public/images/').'normal/'.$newFilename.$originalExtension, 75);         
+		// //Создаем обычный вариант
+  //       $img = Image::make(Storage::path('/public/images/').'origin/'.$newFilename.$originalExtension);
+  //       $img->fit(1200, 1200); // обрезаем до
+		// $img->save(Storage::path('/public/images/').'normal/'.$newFilename.$originalExtension, 75);         
 
-        // webp
-        $imgWebp = $img->encode('webp', 75);  // 75 is image quality and its value can be 1 to 100
-		$imgWebp->save(Storage::path('/public/images/').'normal/'.$newFilename.'.webp'); // 75 is the image quality
+  //       // webp
+  //       $imgWebp = $img->encode('webp', 75);  // 75 is image quality and its value can be 1 to 100
+		// $imgWebp->save(Storage::path('/public/images/').'normal/'.$newFilename.'.webp'); // 75 is the image quality
 
 
-        //Создаем миниатюру изображения и сохраняем ее
-        $thumbnail = Image::make(Storage::path('/public/images/').'origin/'.$newFilename.$originalExtension);
-        $thumbnail->fit(640, 640);
-        $thumbnail->save(Storage::path('/public/images/').'thumbnail/'.$newFilename.$originalExtension, 75);// 75 is the image quality
+  //       //Создаем миниатюру изображения и сохраняем ее
+  //       $thumbnail = Image::make(Storage::path('/public/images/').'origin/'.$newFilename.$originalExtension);
+  //       $thumbnail->fit(640, 640);
+  //       $thumbnail->save(Storage::path('/public/images/').'thumbnail/'.$newFilename.$originalExtension, 75);// 75 is the image quality
 
-        // webp
-        $thumbnailWebp = $img->encode('webp', 75);  // 75 is image quality and its value can be 1 to 100
-		$thumbnailWebp->save(Storage::path('/public/images/').'thumbnail/'.$newFilename.'.webp'); // 75 is the image quality
+  //       // webp
+  //       $thumbnailWebp = $img->encode('webp', 75);  // 75 is image quality and its value can be 1 to 100
+		// $thumbnailWebp->save(Storage::path('/public/images/').'thumbnail/'.$newFilename.'.webp'); // 75 is the image quality
 
         // $save = new Photo;
  
